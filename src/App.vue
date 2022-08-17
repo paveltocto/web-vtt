d70b50c11a4c2d1edad397a2c32fbdfb8c9a11e8
Pavel <neo2318@gmail.com>
2022-08-16 22:06:26 -0500

    config firebase

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3>Web VTT</h3>
+  <h3>Vue Audio VTT</h3>
   <player-audio :audio="require('@/assets/ja-bt-civics-adv-citizen-audio001.mp3')"
                 :track="require('@/assets/ja-bt-civics-adv-citizen-audio001.vtt')" >
     <player-audio-caption></player-audio-caption>
